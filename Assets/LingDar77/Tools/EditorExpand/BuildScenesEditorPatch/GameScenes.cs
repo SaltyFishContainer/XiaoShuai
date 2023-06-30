503118edfed3f5db0648a118b0830cf7e00b4796
ChenMoXuan <chenmoxuan7@gmail.com>
2023-06-30 20:48:52 +0800

    新增Skip页面、challenge页面、更改了Main、Pause、Save的命名和部分样式

--- Assets/LingDar77/Tools/EditorExpand/BuildScenesEditorPatch/GameScenes.cs
+++ Assets/LingDar77/Tools/EditorExpand/BuildScenesEditorPatch/GameScenes.cs
@@ -3,18 +3,12 @@ namespace Lingdar77.Functional
     public class GameScenes
     {
         public static readonly string[] names;
-        public static readonly string STARTUP = "Startup";
-        public static readonly uint STARTUP_INDEX = 0;
-        public static readonly string SPRING = "Spring";
-        public static readonly uint SPRING_INDEX = 1;
-        public static readonly string AKI = "Aki";
-        public static readonly uint AKI_INDEX = 2;
+        public static readonly string UI_TEST = "UI Test";
+        public static readonly uint UI_TEST_INDEX = 0;
         static GameScenes()
         {
-            names = new string[3];
-            names[0] = "Startup";
-            names[1] = "Spring";
-            names[2] = "Aki";
+            names = new string[1];
+            names[0] = "UI Test";
         }
     }
 }
