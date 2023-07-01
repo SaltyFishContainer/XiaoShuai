9c5c73ab85e5fcca6904dacc28f8749de87493ce
LingDar77 <779951546@qq.com>
2023-07-01 16:23:23 +0800

    ✨ feat: complete some pages

--- Assets/Assets/App/Scripts/MainMenuManager.cs
+++ Assets/Assets/App/Scripts/MainMenuManager.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameController gameController;
+    [SerializeField] private GameObject messageBoxQuit;
+    [SerializeField] private Button confirmQuitButton;
+    [SerializeField] private Button cancelQuitButton;
 
 
+    private void Awake()
+    {
+        confirmQuitButton.onClick.AddListener(() => { Application.Quit(); });
+        cancelQuitButton.onClick.AddListener(() => { messageBoxQuit.SetActive(false); });
+    }
     public void PlayNewGame()
     {
         gameController.StartPlay();
     }
-    public void ContinueLastGame()
-    {
-
-    }
     public void ExitGame()
     {
-        Application.Quit();
+        messageBoxQuit.SetActive(true);
     }
 }
