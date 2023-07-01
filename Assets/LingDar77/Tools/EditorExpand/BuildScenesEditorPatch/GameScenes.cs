ea019ab9c9be5667e201aa9f5e027979e6104155
LingDar77 <779951546@qq.com>
2023-07-01 21:54:45 +0800

    增加了文本，Game界面Back按钮改为FlowChart，Main界面添加Thanks按钮

--- Assets/LingDar77/Tools/EditorExpand/BuildScenesEditorPatch/GameScenes.cs
+++ Assets/LingDar77/Tools/EditorExpand/BuildScenesEditorPatch/GameScenes.cs
@@ -3,12 +3,12 @@ namespace Lingdar77.Functional
     public class GameScenes
     {
         public static readonly string[] names;
-        public static readonly string UI_TEST = "UI Test";
-        public static readonly uint UI_TEST_INDEX = 0;
+        public static readonly string MAINGAME = "MainGame";
+        public static readonly uint MAINGAME_INDEX = 0;
         static GameScenes()
         {
             names = new string[1];
-            names[0] = "UI Test";
+            names[0] = "MainGame";
         }
     }
 }
